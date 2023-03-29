69baafa943d5a909ba047b43ba2772c824702e9a
Sirumba <simonsmaina45@gmail.com>
2023-03-29 11:17:03 +0300

    completed

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
+require "pry"
+
 class Person
 
-end
+    def name=(value)
+        @name = value
+    end
+
+    def name
+        @name
+    end
+
+    def job=(value)
+        @job = value
+    end
+
+    def job
+        @job
+    end
+
